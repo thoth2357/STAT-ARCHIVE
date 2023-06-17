896dd8c8932fbdb71e356dd996bae85f627edd34
Thoth <thoth2357@gmail.com>
2023-06-17 10:50:53 +0100

    increased password length of login page to match same as register

--- Authentication/forms.py
+++ Authentication/forms.py
@@ -6,7 +6,7 @@ import re
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150, required=True,widget=forms.TextInput(attrs={'class': 'input100', 'placeholder': 'Matric Number'}))
-    password = forms.CharField(max_length=8, required=True,widget=forms.PasswordInput(attrs={'class': 'input100', 'placeholder': 'Password'}))
+    password = forms.CharField(max_length=100, required=True,widget=forms.PasswordInput(attrs={'class': 'input100', 'placeholder': 'Password'}))
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=100, required=True,widget=forms.TextInput(attrs={'class': 'input100', 'placeholder': 'Email'}))
