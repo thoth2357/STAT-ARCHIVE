2cdaf78040b4eb0e676aabe88c8e3dfceac14c66
Thoth <thoth2357@gmail.com>
2023-06-17 10:27:36 +0100

    removed code in authentication model as django predefined user model was used

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class User(models.Model):
-    fullname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username
