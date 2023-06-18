9c9f0db0e00748d24d43c744b0ad9e443c12cf05
Thoth <thoth2357@gmail.com>
2023-06-18 10:20:02 +0100

    added email not verified check to login view, added username or email already exist check to register view, added generate token and verification link feature and email sending for verification, added verifyemail view to handle mail verification

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,11 +1,13 @@
 
 # Create your views here.
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import JsonResponse
 from .forms import LoginForm, RegisterForm
+from .tasks import send_verification_email  
+from .utils import generate_token, generate_link, decode_token
+from .models import User
 class LoginView(View):
     def get(self, request):
         form = LoginForm()
@@ -16,10 +18,14 @@ class LoginView(View):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return JsonResponse({'success': True})  # Return success response
+            if user.is_active == False:
+                return JsonResponse({'error': 'Email is not verified'}, status=400)
+            else:
+                login(request, user)
+                return JsonResponse({'success': True})  # Return success response
         else:
             return JsonResponse({'error': 'Invalid username or password'}, status=400)  # Return error response
+            
 
 class RegisterView(View):
     def get(self, request):
@@ -29,7 +35,42 @@ class RegisterView(View):
     def post(self, request):
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return JsonResponse({'success': 'Registration Successful, Check Email to Verify'}, status=200)  # Return error response
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            
+            # Check if username or email already exists
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({'error': 'Username already exists'}, status=400)
+            elif User.objects.filter(email=email).exists():
+                return JsonResponse({'error': 'Email already exists'}, status=400)
+            
+            user = form.save(commit=False)
+            user.is_active = False  # Set the user as inactive until email verification
+            user.save()
+
+            # Generate verification token
+            uid, token = generate_token(user)
+
+            # Construct the verification URL
+            verification_link = generate_link(request, uid, token)
+
+            print(verification_link, 'link')
+            
+            # Send verification email asynchronously
+            # send_verification_email.delay(user.email, 'Account Verification' ,verification_link) #TODO Schedule the email sending task asynchronously
+            return JsonResponse({'success': 'Registration Successful, Check Email to Verify'}, status=200)  # Return sucess response
         else:
-            return JsonResponse({'error': 'Error In Registration'}, status=400)  # Return error response
+            return JsonResponse({'error': f'Username or Email Already Exists'}, status=400)  # Return error response
+
+class VerifyEmailView(View):
+    def get(self, request, uidb64, token):
+        try:
+            user,token_generator =  decode_token(uidb64)
+            if token_generator.check_token(user, token):
+                user.is_active = True
+                user.save()
+                return render(request, 'verification_sucess.html')
+            else:
+                return render(request, 'verification_error.html')  
+        except (TypeError, ValueError, OverflowError, Exception):
+            return render(request, 'verification_error.html')  
