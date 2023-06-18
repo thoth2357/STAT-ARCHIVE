2ac3b5e96d7545e39f20f9d6c9ffb88dc3ae2421
Thoth <thoth2357@gmail.com>
2023-06-18 10:15:07 +0100

    add verify url to registered urls

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include # new
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, VerifyEmailView
 
 urlpatterns = [
     path("", LoginView.as_view(), name="login"),
-    path("register/", RegisterView.as_view(), name="register")
+    path("register/", RegisterView.as_view(), name="register"),
+    path("verify/<uidb64>/<token>/", VerifyEmailView.as_view(), name="verify_email")
 ]
