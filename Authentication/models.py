8949def1251e56d4857d27b19bdc9de4b6d1dbf5
Thoth <thoth2357@gmail.com>
2023-06-18 09:51:45 +0100

    created new user model and registered it in admin

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,1 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class User(AbstractUser):
+    fullname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
