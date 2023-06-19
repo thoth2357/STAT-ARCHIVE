c7cf5912a1f523be273734eba2bb6d014c9f1f58
Thoth <thoth2357@gmail.com>
2023-06-19 11:18:22 +0100

    added logout url to urlpatterns

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include # new
-from .views import LoginView, RegisterView, VerifyEmailView, ForgotPasswordView,ResetPasswordView
+from .views import LoginView, RegisterView, VerifyEmailView, ForgotPasswordView,ResetPasswordView,LogoutView
 
 urlpatterns = [
     path("", LoginView.as_view(), name="login"),
@@ -7,5 +7,5 @@ urlpatterns = [
     path("verify/<uidb64>/<token>/", VerifyEmailView.as_view(), name="verify_email"),
     path('forgot-password/', ForgotPasswordView.as_view(), name='forgot_password'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
-    
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
