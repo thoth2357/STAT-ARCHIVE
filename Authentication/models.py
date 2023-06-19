fd2b3a1e6d912bb068eeab1c13b5f8f2a0656770
Thoth <thoth2357@gmail.com>
2023-06-19 08:39:30 +0100

    added reset token and expiration to user model

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     fullname = models.CharField(max_length=100)
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+    reset_token = models.CharField(max_length=100, blank=True, null=True)
+    reset_token_expiration = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.username
