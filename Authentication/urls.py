0ceed57e44382f09d3d307d1b708400459817698
Thoth <thoth2357@gmail.com>
2023-06-19 08:40:21 +0100

    added forgot password, reset password to url patterns

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include # new
-from .views import LoginView, RegisterView, VerifyEmailView
+from .views import LoginView, RegisterView, VerifyEmailView, ForgotPasswordView,ResetPasswordView
 
 urlpatterns = [
     path("", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
-    path("verify/<uidb64>/<token>/", VerifyEmailView.as_view(), name="verify_email")
+    path("verify/<uidb64>/<token>/", VerifyEmailView.as_view(), name="verify_email"),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot_password'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
+    
 ]
