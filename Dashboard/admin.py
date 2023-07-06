5bebd805b056be4038c8d2f8a72ec3b19f2b6a99
Thoth <thoth2357@gmail.com>
2023-07-06 17:44:23 +0100

    Register models in the Django admin site
    - Added the PastQuestion, TextBook, and Project models to the Django admin site
    - Enabled the admin interface for managing these models

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import PastQuestion, TextBook, Project
 # Register your models here.
+admin.site.register(PastQuestion)
+admin.site.register(TextBook)
+admin.site.register(Project)
