986220e42fae3c269945ff982090f81caf61b30d
Thoth <thoth2357@gmail.com>
2023-07-06 11:30:02 +0100

    "Add file upload functionality and progress bar, handle success and error cases Modified existing code to handle file uploads using AJAX and FormData Implemented a progress bar to display the upload progress Included success and error handlers for the upload process Added functions to collect form data for different upload types Retrieved CSRF token and included it in AJAX request headers"

--- static/assets/js/dashboard.js
+++ static/assets/js/dashboard.js
@@ -1,19 +1,157 @@
 $(document).ready(function () {
+    // Retrieve the CSRF token
+    var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+
     // Hide all upload forms initially except the active one
     $(".upload-form").not(".active").hide();
 
     // Handle click event on upload type buttons
     $(".upload-type-btn").click(function () {
-      // Remove active class from all buttons
-      $(".upload-type-btn").removeClass("active");
-      // Add active class to the clicked button
-      $(this).addClass("active");
+        // Remove active class from all buttons
+        $(".upload-type-btn").removeClass("active");
+        // Add active class to the clicked button
+        $(this).addClass("active");
 
-      // Hide all upload forms
-      $(".upload-form").hide();
+        // Hide all upload forms
+        $(".upload-form").hide();
 
-      // Show the corresponding form based on data-target attribute
-      var targetForm = $(this).attr("data-target");
-      $("#" + targetForm).show();
+        // Show the corresponding form based on data-target attribute
+        var targetForm = $(this).attr("data-target");
+        $("#" + targetForm).show();
     });
-  });
+
+    // Handle click event on upload buttons
+    $(".upload-btn").click(function () {
+        console.log("Upload button clicked!");
+        // Get the form id of the active upload form
+        var formId = $(".upload-type-btn.active").attr("data-target");
+        console.log(formId);
+        var formData = new FormData();
+
+        // Get the query parameter based on the form id
+        var queryParam = "";
+        if (formId === "pastQuestionsForm") {
+            queryParam = "pq";
+            formData = formPastQuestionsData();
+        } else if (formId === "textbookForm") {
+            queryParam = "txb";
+            formData = formTextbookData();
+        } else if (formId === "projectForm") {
+            queryParam = "prj";
+            formData = formProjectData();
+        }
+
+        // Display the progress bar
+        var progressBar = $(".upload-progress .progress-bar");
+        progressBar.css("width", "0%");
+        progressBar.text("0%");
+        progressBar.addClass("progress-bar-striped active");
+        $(".upload-progress").show(); // Show the progress bar container
+
+        // Send the form data to the server
+        $.ajax({
+            url: "/Stat-Archive/upload/?query=" + queryParam,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            },
+            xhr: function () {
+                var xhr = new window.XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function (event) {
+                    if (event.lengthComputable) {
+                        var progress = Math.round((event.loaded / event.total) * 100);
+                        progressBar.css("width", progress + "%");
+                        progressBar.text(progress + "%");
+                        console.log(progress + "in percent %");
+                    }
+                });
+                return xhr;
+            },
+            success: function (response) {
+                //close modal
+                var modal_close = document.getElementById("close-btn"); // Hide the upload modal
+                modal_close.click();
+
+                // Display the success message
+                Swal.fire({
+                    icon: "success",
+                    title: "Upload Successful!",
+                    text: "Thanks Your file has been uploaded successfully!",
+                });
+
+                confetti({
+                    particleCount: 500,
+                    spread: 100,
+                    origin: { y: 0.6 },
+                });
+            },
+            error: function (xhr, status, error) {
+                //close modal
+                var modal_close = document.getElementById("close-btn"); // Hide the upload modal
+                modal_close.click();
+
+                // Display the success message
+                Swal.fire({
+                    icon: "error",
+                    title: "Upload Not Successful!",
+                    text: "Your file hasnt been uploaded, Try again!. If problem persist contact admin@sta-archive.com",
+                });
+            },
+        });
+    });
+
+    // Function to form the data for the past questions form
+    function formPastQuestionsData() {
+        var session = $("#sessionDropdown").val();
+        var pastQuestionsType = $("#sessionDropdowntype").val();
+        var pastQuestionsLecturerName = $("#pastQuestionsLecturerName").val();
+        var pastQuestionsCourseName = $("#pastQuestionsCourseName").val();
+        var pastQuestionsCourseCode = $("#pastQuestionsCourseCode").val();
+        var pastQuestionFile = $("#pastQuestionsFileInput").prop("files")[0];
+
+        var formData = new FormData();
+        formData.append("session", session);
+        formData.append("PastQuestionsType", pastQuestionsType);
+        formData.append("LecturerName", pastQuestionsLecturerName);
+        formData.append("CourseName", pastQuestionsCourseName);
+        formData.append("CourseCode", pastQuestionsCourseCode);
+        formData.append("QuestionFile", pastQuestionFile);
+
+        return formData;
+    }
+
+    // Function to form the data for the textbook form
+    function formTextbookData() {
+        var textbookName = $("#textbookName").val();
+        var textbookAuthor = $("#textbookAuthor").val();
+        var textbookFile = $("#textbookFileInput").prop("files")[0];
+
+        var formData = new FormData();
+        formData.append("textbookName", textbookName);
+        formData.append("textbookAuthor", textbookAuthor);
+        formData.append("textbookFile", textbookFile);
+
+        return formData;
+    }
+
+    // Function to form the data for the project form
+    function formProjectData() {
+        var session = $("#sessionDropdown").val();
+        var projectTopic = $("#projectTopic").val();
+        var projectAuthor = $("#projectAuthor").val();
+        var projectSupervisor = $("#projectSupervisor").val();
+        var projectFile = $("#projectFileInput").prop("files")[0];
+
+        var formData = new FormData();
+        formData.append("session", session);
+        formData.append("projectTopic", projectTopic);
+        formData.append("projectAuthor", projectAuthor);
+        formData.append("projectSupervisor", projectSupervisor);
+        formData.append("projectFile", projectFile);
+
+        return formData;
+    }
+});
