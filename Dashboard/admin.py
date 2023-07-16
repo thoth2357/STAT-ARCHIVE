d27dc6b3168456b6f518efd057cb61d83ce2772b
thoth2357 <thoth2357@gmail.com>
2023-07-16 23:38:44 +0100

    "Added 'Sessions' model registration to Django admin interface
    Registered the 'Sessions' model in the Django admin interface, enabling admins to manage individual session entries.
    All existing models ('PastQuestion', 'TextBook', and 'Project') remain registered for easy administration.

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import PastQuestion, TextBook, Project
+from .models import PastQuestion, TextBook, Project, Sessions
 # Register your models here.
 admin.site.register(PastQuestion)
 admin.site.register(TextBook)
 admin.site.register(Project)
+admin.site.register(Sessions)
+
