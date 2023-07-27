1263fd18b4b37f2fd2ad18116a97c67ce7226ed8
thoth2357 <thoth2357@gmail.com>
2023-07-27 14:25:36 +0100

    added url for reporting resources

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import Bibliotheca, UploadResources, ResourcesSearch
+from .views import Bibliotheca, UploadResources, ResourcesSearch, ResourcesReportView
 
 urlpatterns = [
     path("", Bibliotheca.as_view(), name="Bibliotheca"),
     path("upload/", UploadResources.as_view(), name="upload"),
-    path("resource-search/", ResourcesSearch.as_view(), name="resource-search")
-    
+    path("resource-search/", ResourcesSearch.as_view(), name="resource-search"),
+    path("report/", ResourcesReportView.as_view(), name="report"),
 ]
