203ec0a982a1983a002d93d581b333b87c41d40c
thoth2357 <thoth2357@gmail.com>
2023-08-03 21:09:35 +0100

    :recycle: refactor celery to use lambda function for autodisocering tasks

--- statArchive/celery.py
+++ statArchive/celery.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from statArchive.celery import Celery
+from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statArchive.settings.dev')
 
 app = Celery('statArchive')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks() # Load task modules from all registered Django app configs.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
