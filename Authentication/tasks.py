c30b2975eaa7bd63d9cc00964bf8b4f87e8f45da
thoth2357 <thoth2357@gmail.com>
2023-08-04 00:51:48 +0100

    :recycle: changed username from email to user fullname and added condition to know it send email is sending a reset or verification email to display appropriate mail template

--- Authentication/tasks.py
+++ Authentication/tasks.py
@@ -9,17 +9,19 @@ from Log.models import Log
 
 
 @shared_task(bind=True, max_retries=3)
-def send_email(self, email, subject, message):
+def send_email_func(self, user, email, subject, message, type_):
     try:
         context = {
             
-            'username': email.split('@')[0],
+            'username': user,
             'verification_link': message
         }
         # Code to send the verification email
         # Use Django's send_mail() function or any email sending library of your choice
-        
-        html_message = render_to_string('Auth/verify_email.html', context)
+        if type_ == "verify":
+            html_message = render_to_string('Auth/verify_email.html', context)
+        else:
+            html_message = render_to_string('Auth/forgot_password_email.html', context)
         # print(html_message, "message-html")
         send_mail(subject, message="", from_email=settings.DEFAULT_FROM_EMAIL, recipient_list=[email], html_message=html_message)
 
